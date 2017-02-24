9f8b0063f625d431e1020ff01dd9ffb77ac76ba0
Johannes Tandler <johannes@jtdl.de>
2017-02-25 00:01:53 +0100

    allow mavenization of multiple repositories
    Signed-off-by: Johannes Tandler <johannes@jtdl.de>

--- repository/gradle.mavenize/src/main/groovy/de/monkeyworks/buildmonkey/mavenizer/MavenizerPlugin.groovy
+++ repository/gradle.mavenize/src/main/groovy/de/monkeyworks/buildmonkey/mavenizer/MavenizerPlugin.groovy
@@ -4,68 +4,82 @@ import de.monkeyworks.buildmonkey.mavenizer.mavenize.BundleMavenDeployer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+
+class MavenizeTaskExtension {
+    String sourceP2Repository
+    String targetDir
+    String groupId
+    boolean useP2MirrorOutput
+
+    Project project
+
+    MavenizeTaskExtension(Project project) {
+        groupId = "eclipse"
+        targetDir = "build/m2-repository"
+        sourceP2Repository = ""
+        useP2MirrorOutput = false
+        this.project = project
+    }
+}
+
+class MavenizeExtension {
+    def mavenizeTasks = []
+    def project
+
+    MavenizeExtension(Project project) {
+        this.project = project
+    }
+
+    void mavenizeTask(Closure closure) {
+        def task = new MavenizeTaskExtension()
+        project.configure(task, closure)
+        mavenizeTasks.add(task)
+    }
+}
+
 class MavenizerPlugin implements Plugin<Project> {
 
 	static final String TASK_NAME_CONVERT_P2_M2 = "mavenizeP2Repository"
 	// Must be a common constant
 	static final String TASK_NAME_MIRROR_P2 = "mirrorP2Repository"
 
-	static class MavenizeExtension {
-		def sourceP2Repository
-		def targetDir
-
-		def groupId
-
-	    MavenizeExtension() {
-	    	groupId = "eclipse"
-	    	targetDir = "build/m2-repository"
-	    	sourceP2Repository = ""
-	    }
-	}
-
 	@Override
     public void apply(Project project) {
-        project.extensions.create("mavenize", MavenizeExtension)
+        project.extensions.create("mavenize", MavenizeExtension, project)
 
-        // if eclipse plugin is configured, use it as source path 
+        def name = TASK_NAME_CONVERT_P2_M2
+        project.tasks.create(name)
+
         project.afterEvaluate {
-			if(project.tasks.findByPath(TASK_NAME_MIRROR_P2) != null) {
-				project.mavenize.sourceP2Repository = "$project.buildDir/p2-repository" 					
-			}
-		}
+            MavenizeExtension config = project.mavenize
+            // create clan task if necessary
+            if (project.tasks.findByPath('clean') == null) {project.tasks.create('clean')}
 
-        addTaskToConvertP2ToMaven(project)
-    }
+            def i = 1
+            for(MavenizeTaskExtension task : config.mavenizeTasks) {
+                def cleanTask = project.task("cleanM2Repository_$i") {
+                    doFirst {
+                        MavenizeExtension parameter = project.mavenize
+                        new File(task.targetDir).deleteDir()
+                    }
+                }
+                project.tasks.clean.dependsOn cleanTask
 
-    static void addTaskToConvertP2ToMaven(Project project) {
-        project.task('cleanM2Repository') {
-            doFirst {
-				MavenizeExtension parameter = project.mavenize
-                new File(parameter.targetDir).deleteDir()
+                project.task(name + "_$i", dependsOn: [name]) {
+                    description = "Converts created p2 repository into m2 repository"
+
+                    if(project.tasks.findByPath(TASK_NAME_MIRROR_P2) != null) {
+                        task.sourceP2Repository = "$project.buildDir/p2-repository"
+                    }
+
+                    doLast {
+                        def converter = new BundleMavenDeployer(project.ant, task.groupId, project.logger)
+                        converter.deploy(new File(task.sourceP2Repository), new File(task.targetDir))
+                    }
+                }
+
+                i++
             }
         }
-        if (project.tasks.findByPath('clean') == null) {project.tasks.create('clean')}
-        project.tasks.clean.dependsOn 'cleanM2Repository'
-
-    	project.task(TASK_NAME_CONVERT_P2_M2) {
-    		description = "Converts created p2 repository into m2 repository"
-
-			MavenizeExtension parameter = project.mavenize
-
-			// why does this project depend on downloading a p2 repository?
-    		project.afterEvaluate {
-	    		if(project.tasks.findByPath(TASK_NAME_MIRROR_P2) != null) {
-	    			if(project.mavenize.sourceP2Repository.equals("$project.buildDir/p2-repository")) {
-						dependsOn TASK_NAME_MIRROR_P2
-					}
-				}
-			}
-
-    		doLast {
-    			def converter = new BundleMavenDeployer(project.ant, parameter.groupId, project.logger)
-    			converter.deploy(new File(parameter.sourceP2Repository), new File(parameter.targetDir))
-    		}
-    	}
     }
-
 }
