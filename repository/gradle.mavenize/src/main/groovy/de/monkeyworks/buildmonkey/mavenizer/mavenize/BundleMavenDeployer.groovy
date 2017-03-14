dd9616d6029f8496f3a8aff2d9c7725042a7bff9
Johannes Tandler <johannes@jtdl.de>
2017-03-14 17:59:32 +0100

    deploy features to m2 repositories when mavenizing p2 repositories
    Signed-off-by: Johannes Tandler <johannes@jtdl.de>

--- repository/gradle.mavenize/src/main/groovy/de/monkeyworks/buildmonkey/mavenizer/mavenize/BundleMavenDeployer.groovy
+++ repository/gradle.mavenize/src/main/groovy/de/monkeyworks/buildmonkey/mavenizer/mavenize/BundleMavenDeployer.groovy
@@ -20,156 +20,174 @@ import org.gradle.api.logging.Logger
  * Converts an Eclipse update site into a Maven repository.
  */
 final class BundleMavenDeployer {
-  private final AntBuilder ant
-  private final String groupIdForBundles
-  private final Logger logger
+    private final AntBuilder ant
+    private final String groupIdForBundles
+    private final Logger logger
 
-  private Map artifacts = [:]
-  private Map artifactsNl = [:]
-  private Map artifactFiles = [:]
-  private Map sourceFiles = [:]
+    private Map artifacts = [:]
+    private Map artifactsNl = [:]
+    private Map artifactFiles = [:]
+    private Map sourceFiles = [:]
 
-  /**
-   * Creates a new instance.
-   *
-   * @param ant ant build instance to execute the deployment task defined in the maven-ant-tasks dependency
-   * @param groupIdForBundles the maven groupId where the eclipse plugins will be published
-   */
-  BundleMavenDeployer(AntBuilder ant, String groupIdForBundles, Logger logger = null) {
-    this.ant = ant
-    this.groupIdForBundles = groupIdForBundles
-    this.logger = logger
-  }
-
-  /**
-   * Publishes Eclipse bundles from an update site to a Maven repository.
-   *
-   * @param source the update site location where the eclipse bundles are located
-   * @param target the target maven repository location where the bundles will be deployed
-   */
-  void deploy(File source, File target) {
-    File pluginFolder = new File(source, 'plugins')
-    assert pluginFolder.exists()
-
-    collectArtifacts(pluginFolder)
-    fixDependencies()
-    deployBundles(new DeployMavenAntTaskExecutor(ant, target))
-
-    File dependencyVersionFile = new File(target.getAbsolutePath() + "/../bundle.versions")
-
-    Properties props = new Properties()
-    artifacts.each { id, poms ->
-      props.setProperty(id, poms.last().version)
+    /**
+     * Creates a new instance.
+     *
+     * @param ant ant build instance to execute the deployment task defined in the maven-ant-tasks dependency
+     * @param groupIdForBundles the maven groupId where the eclipse plugins will be published
+     */
+    BundleMavenDeployer(AntBuilder ant, String groupIdForBundles, Logger logger = null) {
+        this.ant = ant
+        this.groupIdForBundles = groupIdForBundles
+        this.logger = logger
     }
-    dependencyVersionFile.newWriter().withWriter { w ->
-      props.store(w, null)
-    }
-  }
 
-  private void collectArtifacts(artifactsSourceDir) {
-    def processFile = { File file ->
-      logger.info("Collecting artifacts: ${file.name}")
-      try {
-        Bundle2Pom reader = new Bundle2Pom(group: groupIdForBundles, dependencyGroup: groupIdForBundles)
-        Pom pom = reader.convert(file)
-        def source_match = pom.artifact =~ /(.*)\.source/
-        if (source_match) {
-          def artifact = source_match[0][1]
-          sourceFiles["${artifact}:${pom.version}"] = file
-        } else {
-          def nl_match = pom.artifact =~ /(.*)\.nl_(.*)/
-          if (nl_match) {
-            def artifact = nl_match[0][1]
-            def language = nl_match[0][2]
-            if (!artifactsNl[language])
-              artifactsNl[language] = [:]
-            artifactsNl[language][artifact] = pom
-          } else {
+    /**
+     * Publishes Eclipse bundles from an update site to a Maven repository.
+     *
+     * @param source the update site location where the eclipse bundles are located
+     * @param target the target maven repository location where the bundles will be deployed
+     */
+    void deploy(File source, File target) {
+        File pluginFolder = new File(source, 'plugins')
+        assert pluginFolder.exists()
+
+        File featuresFolder = new File(source, 'features')
+        assert featuresFolder.exists()
+
+        collectArtifacts(pluginFolder)
+        collectFeatures(featuresFolder)
+
+        fixDependencies()
+
+        deployBundles(new DeployMavenAntTaskExecutor(ant, target))
+
+    }
+
+    private void collectFeatures(featuresDir) {
+        def processFeature = { File file ->
+            Feature2Pom reader = new Feature2Pom(group: groupIdForBundles, dependencyGroup: groupIdForBundles)
+            Pom pom = reader.convert(file)
+
             if (!artifacts.containsKey(pom.artifact))
-              artifacts[pom.artifact] = []
+                artifacts[pom.artifact] = []
             artifacts[pom.artifact].add pom
-          }
-          artifactFiles["${pom.artifact}:${pom.version}"] = file
-        }
-      } catch (Exception e) {
-        logger.info("Error while mavenizing ${file}")
-        e.printStackTrace()
-      }
-    }
-    logger.info("Reading bundles in $artifactsSourceDir")
-    try {
-      artifactsSourceDir.eachDir processFile
-      artifactsSourceDir.eachFileMatch ~/.*\.jar/, processFile
-    } finally {
-      logger.info("Finished reading bundles in $artifactsSourceDir")
-    }
-  }
+            artifactFiles["${pom.artifact}:${pom.version}"] = file
 
-  private void fixDependencies() {
-    logger.info('Fixing dependencies')
-    try {
-      artifacts.each { name, artifactVersions ->
-        logger.info("Fixing dependencies: $name")
-        artifactVersions.each { pom ->
-          pom.dependencyBundles.removeAll { reqBundle ->
-            if (!artifacts[reqBundle.name.trim()]) {
-              logger.info("Warning: artifact dependency $pom.group:$pom.artifact:$pom.version -> $reqBundle.name could not be resolved.")
-              return true
-            }
-            return false
-          }
-          pom.dependencyBundles.each { reqBundle ->
-            def resolvedVersions = artifacts[reqBundle.name.trim()]
-            if (resolvedVersions.size() == 1)
-              reqBundle.version = resolvedVersions[0].version
-            else if (!resolvedVersions.find { it -> it.version == reqBundle.version.trim() }) {
-              def compare = { a, b -> new Version(a).compare(new Version(b)) }
-              resolvedVersions = resolvedVersions.sort(compare)
-              int i = Collections.binarySearch resolvedVersions, reqBundle.version.trim(), compare as java.util.Comparator
-              if (i < 0)
-                i = -i - 1
-              if (i > resolvedVersions.size() - 1)
-                i = resolvedVersions.size() - 1
-              def c = resolvedVersions[i]
-              def depsStr = resolvedVersions.collect({ p -> "$p.group:$p.artifact:$p.version" }).join(', ')
-              logger.info("Warning: resolved ambiguous dependency: $pom.group:$pom.artifact:$pom.version -> $reqBundle.name:$reqBundle.version, chosen $c.group:$c.artifact:$c.version from [$depsStr].")
-              reqBundle.version = c.version
-            }
-          }
-          artifactsNl.each { language, map_nl ->
-            def pom_nl = map_nl[pom.artifact]
-            if (pom_nl)
-              pom.dependencyBundles.each { dep_bundle ->
-                def dep_pom_nl = map_nl[dep_bundle.name]
-                if (dep_pom_nl) {
-                  pom_nl.dependencyBundles.add new DependencyBundle(name: dep_pom_nl.artifact, version: dep_pom_nl.version, visibility: dep_bundle.visibility, resolution: dep_bundle.resolution)
+        }
+        logger.info("Reading features in $featuresDir")
+
+        try {
+            featuresDir.eachDir processFeature
+            featuresDir.eachFileMatch ~/.*\.jar/, processFeature
+        } finally {
+            logger.info("Finished reading bundles in $featuresDir")
+        }
+    }
+
+    private void collectArtifacts(artifactsSourceDir) {
+        def processFile = { File file ->
+            logger.info("Collecting artifacts: ${file.name}")
+            try {
+                Bundle2Pom reader = new Bundle2Pom(group: groupIdForBundles, dependencyGroup: groupIdForBundles)
+                Pom pom = reader.convert(file)
+                def source_match = pom.artifact =~ /(.*)\.source/
+                if (source_match) {
+                    def artifact = source_match[0][1]
+                    sourceFiles["${artifact}:${pom.version}"] = file
+                } else {
+                    def nl_match = pom.artifact =~ /(.*)\.nl_(.*)/
+                    if (nl_match) {
+                        def artifact = nl_match[0][1]
+                        def language = nl_match[0][2]
+                        if (!artifactsNl[language])
+                            artifactsNl[language] = [:]
+                        artifactsNl[language][artifact] = pom
+                    } else {
+                        if (!artifacts.containsKey(pom.artifact))
+                            artifacts[pom.artifact] = []
+                        artifacts[pom.artifact].add pom
+                    }
+                    artifactFiles["${pom.artifact}:${pom.version}"] = file
                 }
-              }
-          }
+            } catch (Exception e) {
+                logger.info("Error while mavenizing ${file}")
+                e.printStackTrace()
+            }
         }
-      }
-    } finally {
-      logger.info('Finished fixing dependencies')
+        logger.info("Reading bundles in $artifactsSourceDir")
+        try {
+            artifactsSourceDir.eachDir processFile
+            artifactsSourceDir.eachFileMatch ~/.*\.jar/, processFile
+        } finally {
+            logger.info("Finished reading bundles in $artifactsSourceDir")
+        }
     }
-  }
 
-  private deployBundles(DeployMavenAntTaskExecutor executor) {
-    logger.info('Deploying artifacts')
-    try {
-      artifacts.each { name, artifactVersions ->
-        artifactVersions.each { pom ->
-          executor.deployBundle pom, artifactFiles["${pom.artifact}:${pom.version}"], sourceFile: sourceFiles["${pom.artifact}:${pom.version}"]
+    private void fixDependencies() {
+        logger.info('Fixing dependencies')
+        try {
+            artifacts.each { name, artifactVersions ->
+                logger.info("Fixing dependencies: $name")
+                artifactVersions.each { pom ->
+                    pom.dependencyBundles.removeAll { reqBundle ->
+                        if (!artifacts[reqBundle.name.trim()]) {
+                            logger.info("Warning: artifact dependency $pom.group:$pom.artifact:$pom.version -> $reqBundle.name could not be resolved.")
+                            return true
+                        }
+                        return false
+                    }
+                    pom.dependencyBundles.each { reqBundle ->
+                        def resolvedVersions = artifacts[reqBundle.name.trim()]
+                        if (resolvedVersions.size() == 1)
+                            reqBundle.version = resolvedVersions[0].version
+                        else if (!resolvedVersions.find { it -> it.version == reqBundle.version.trim() }) {
+                            def compare = { a, b -> new Version(a).compare(new Version(b)) }
+                            resolvedVersions = resolvedVersions.sort(compare)
+                            int i = Collections.binarySearch resolvedVersions, reqBundle.version.trim(), compare as java.util.Comparator
+                            if (i < 0)
+                                i = -i - 1
+                            if (i > resolvedVersions.size() - 1)
+                                i = resolvedVersions.size() - 1
+                            def c = resolvedVersions[i]
+                            def depsStr = resolvedVersions.collect({ p -> "$p.group:$p.artifact:$p.version" }).join(', ')
+                            logger.info("Warning: resolved ambiguous dependency: $pom.group:$pom.artifact:$pom.version -> $reqBundle.name:$reqBundle.version, chosen $c.group:$c.artifact:$c.version from [$depsStr].")
+                            reqBundle.version = c.version
+                        }
+                    }
+                    artifactsNl.each { language, map_nl ->
+                        def pom_nl = map_nl[pom.artifact]
+                        if (pom_nl)
+                            pom.dependencyBundles.each { dep_bundle ->
+                                def dep_pom_nl = map_nl[dep_bundle.name]
+                                if (dep_pom_nl) {
+                                    pom_nl.dependencyBundles.add new DependencyBundle(name: dep_pom_nl.artifact, version: dep_pom_nl.version, visibility: dep_bundle.visibility, resolution: dep_bundle.resolution)
+                                }
+                            }
+                    }
+                }
+            }
+        } finally {
+            logger.info('Finished fixing dependencies')
         }
-      }
-      artifactsNl.each { language, map_nl ->
-        map_nl.each { artifactName, pom ->
-          executor.deployBundle pom, artifactFiles["${pom.artifact}:${pom.version}"]
-        }
-      }
-    } finally {
-      executor.cleanup()
-      logger.info('Finished deploying artifacts')
     }
-  }
+
+    private deployBundles(DeployMavenAntTaskExecutor executor) {
+        logger.info('Deploying artifacts')
+        try {
+            artifacts.each { name, artifactVersions ->
+                artifactVersions.each { pom ->
+                    executor.deployBundle pom, artifactFiles["${pom.artifact}:${pom.version}"], sourceFile: sourceFiles["${pom.artifact}:${pom.version}"]
+                }
+            }
+            artifactsNl.each { language, map_nl ->
+                map_nl.each { artifactName, pom ->
+                    executor.deployBundle pom, artifactFiles["${pom.artifact}:${pom.version}"]
+                }
+            }
+        } finally {
+            executor.cleanup()
+            logger.info('Finished deploying artifacts')
+        }
+    }
 }
 
---
+++ repository/gradle.mavenize/src/main/groovy/de/monkeyworks/buildmonkey/mavenizer/mavenize/Feature2Pom.groovy
@@ -0,0 +1,71 @@
+package de.monkeyworks.buildmonkey.mavenizer.mavenize
+
+import groovy.util.slurpersupport.GPathResult
+import org.osgi.framework.Constants
+
+import java.util.jar.JarFile
+
+/**
+ * Created by jake on 14/03/2017.
+ */
+class Feature2Pom {
+    String group
+    String dependencyGroup
+
+
+    Feature2Pom(String group = null, String dependencyGroup = null) {
+        this.group = group
+        this.dependencyGroup = dependencyGroup
+    }
+
+    /**
+     * Converts bundle to POM.
+     * @param bundleFileOrDirectory - jar-file or directory containing OSGi bundle.
+     * @return the converted POM.
+     */
+    Pom convert(File bundleFileOrDirectory) {
+        def pom = new Pom()
+        def featurexmlStream
+        def id
+
+        if (bundleFileOrDirectory.isDirectory()) {
+            featurexmlStream = new FileInputStream(new File(bundleFileOrDirectory, 'feature.xml'))
+            pom.packaging = 'dir'
+        } else {
+            def jarFile = new JarFile(bundleFileOrDirectory)
+            featurexmlStream = jarFile.getInputStream(jarFile.getJarEntry("feature.xml"))
+        }
+
+        def parsedXML = new XmlSlurper().parse(featurexmlStream)
+        featurexmlStream.close()
+        id = parsedXML['@id']
+
+        pom.artifact = id
+
+        pom.group = group ?: pom.artifact
+        def version = new Version(parsedXML['@version'].toString())
+        pom.version = "${version.major}.${version.minor}.${version.release}"
+
+        parseDependencies(pom.dependencyBundles, parsedXML)
+
+        return pom
+    }
+
+    private void parseDependencies(List<DependencyBundle> depBundles, GPathResult xml) {
+        def plugins = xml.'**'.findAll{ node-> node.name() == 'plugin'}
+        plugins.forEach { plugin ->
+            DependencyBundle bundle = new DependencyBundle(name: plugin['@id'], resolution: Constants.RESOLUTION_MANDATORY, visibility: Constants.VISIBILITY_PRIVATE, version: "[1.0,)")
+            depBundles.add(bundle)
+        }
+        def required = xml.'**'.findAll{node -> node.name() == 'import'}
+        required.forEach { it ->
+            DependencyBundle bundle = new DependencyBundle(name: it['@plugin'], resolution: Constants.RESOLUTION_MANDATORY, visibility: Constants.VISIBILITY_PRIVATE, version: "[1.0,)")
+            depBundles.add(bundle)
+        }
+        def includes = xml.'**'.findAll{node -> node.name() == 'includes'}
+        includes.forEach { it ->
+            DependencyBundle bundle = new DependencyBundle(name: it['@id'], resolution: Constants.RESOLUTION_MANDATORY, visibility: Constants.VISIBILITY_PRIVATE, version: "[1.0,)")
+            depBundles.add(bundle)
+        }
+    }
+}
